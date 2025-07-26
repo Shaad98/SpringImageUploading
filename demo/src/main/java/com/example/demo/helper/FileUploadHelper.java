b60e9ea4a82fbf32866e9bc1f3dc4d486f65f187
Shaad <141047163+Shaad98@users.noreply.github.com>
2025-07-26 20:06:01 +0530

    File upoading using nio package is done

--- demo/src/main/java/com/example/demo/helper/FileUploadHelper.java
+++ demo/src/main/java/com/example/demo/helper/FileUploadHelper.java
@@ -1,11 +1,14 @@
 package com.example.demo.helper;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+// import java.io.FileOutputStream;
+// import java.io.IOException;
+// import java.io.InputStream;
+// import java.io.OutputStream;
+// import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,17 +21,18 @@ public class FileUploadHelper {
     {
         boolean val = false;
         try {
-            InputStream is = multipartFile.getInputStream();
-            byte[] data = new byte[is.available()];
-            is.read(data);
+            // InputStream is = multipartFile.getInputStream();
+            // byte[] data = new byte[is.available()];
+            // is.read(data);
         
-            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
-            fos.write(data);
+            // FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
+            // fos.write(data);
 
-            fos.flush();
-            fos.close();
-            is.close();
+            // fos.flush();
+            // fos.close();
+            // is.close();
         
+            Files.copy(multipartFile.getInputStream(), Paths.get(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename()),StandardCopyOption.REPLACE_EXISTING);
             val = true;
         } catch (Exception e) {
             e.printStackTrace();
